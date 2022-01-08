f3f6f2d061293577011db7011eebcb591932c267
Lendl Peralta <janlendl@gmail.com>
2022-01-08 19:51:37 +0000

    Add a new route /articles/:id

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "articles#index"
 
   get "/articles", to: "articles#index"
+  get "/articles/:id". to: "articles:show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
