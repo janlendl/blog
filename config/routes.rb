62c97deed65c4b5a86d475d595e018fca6e1d521
Lendl Peralta <janlendl@gmail.com>
2022-01-09 00:30:26 +0000

    Change the routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
