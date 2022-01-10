5719640e9a7954fbb5188fc32d76edcf5c5eb9cd
Lendl Peralta <janlendl@gmail.com>
2022-01-10 04:54:20 +0000

    Fix body not updating

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", 
+    except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -44,6 +48,6 @@ class ArticlesController < ApplicationController
  
   private
     def article_params
-      params.require(:article).permit(:title, :text)
+      params.require(:article).permit(:title, :body)
     end
 end
